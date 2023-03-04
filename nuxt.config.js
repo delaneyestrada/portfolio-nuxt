cd79ed88d3ee614dca84d6eabc952ad565c46152
Delaney Estrada <dillon.estrada55@gmail.com>
2023-03-03 20:24:27 -0600

    Change preview picture

--- nuxt.config.js
+++ nuxt.config.js
@@ -44,7 +44,7 @@ export default {
       {
         hid: "twitter:image",
         name: "twitter:image",
-        content: "https://www.dillonestrada.com/dewebdev.png"
+        content: "https://www.delaneyestrada.com/dewebdev.png"
       },
       {
         hid: "twitter:image:alt",
@@ -65,12 +65,12 @@ export default {
       {
         hid: "og:image",
         property: "og:image",
-        content: "https://www.dillonestrada.com/dewebdev.png"
+        content: "https://www.delaneyestrada.com/dewebdev.png"
       },
       {
         hid: "og:image:secure_url",
         property: "og:image:secure_url",
-        content: "https://www.dillonestrada.com/dewebdev.png"
+        content: "https://www.delaneyestrada.com/dewebdev.png"
       },
       {
         hid: "og:image:alt",
